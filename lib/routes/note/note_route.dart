da41e72116790526264b2d8cd292dbea6b79fab4
Tomasz Marzeion <tmarzeion.napp@gmail.com>
2020-04-07 22:51:28 +0200

    Added note creation on note screen

--- lib/routes/note/note_route.dart
+++ lib/routes/note/note_route.dart
@@ -1,4 +1,6 @@
+import 'package:drawablenotepadflutter/data/notepad_database.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'views/font_picker_menu_item.dart';
 
@@ -10,31 +12,46 @@ class NoteRoute extends StatefulWidget {
 }
 
 class _NoteRouteState extends State<NoteRoute> {
+  final noteTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Note"),
-        actions: <Widget>[
-          FontPickerMenuItem(),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: TextField(
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Write Your note here…'),
-              ),
+    return new WillPopScope(
+        onWillPop: _onWillPop,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Note"),
+            actions: <Widget>[
+              FontPickerMenuItem(),
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: noteTextController,
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Write Your note here…'),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        ));
   }
+
+  Future<bool> _onWillPop() async {
+    final database = Provider.of<NotepadDatabase>(context, listen: false);
+    final noteText = noteTextController.text;
+    if (noteText.isNotEmpty) {
+      database.insertNote(Note(noteText: noteText));
+    }
+    return true;
+  }
+
 }
