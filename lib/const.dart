17a656fa7357249a4a3926e87a400538b24760d5
tmarzeion <marzeion.tomasz@gmail.com>
2020-04-23 10:06:28 +0200

    Const cleanup

--- lib/const.dart
+++ lib/const.dart
@@ -5,20 +5,9 @@ import 'package:flutter/material.dart';
 
 class Constants {
   static final showClearAlertSharedPrefKey = 'showClearAlert';
-  
-  
 }
 
 class Settings {
-  
-  static const int painterPathCompressionLevel = 8;
-  static const paintColorAlpha = 220;
-  static const bottomBarHeight = 50.0;
-  static const undoDisabledColor = Colors.black26;
-  static const defaultThicknessMode = 2;
-  static const bottomBarBackgroundColor = Color.fromARGB(255, 224, 224, 224);
-  static var activeEraserBackground = defaultColor.withAlpha(150);
-  static var defaultColor = Colors.amber;
 
   static List<DrawThicknessMode> drawThicknessModes = [
     DrawThicknessMode(2.0, "assets/1.svg"),
@@ -50,55 +39,50 @@ class Settings {
     Colors.blueGrey.withAlpha(paintColorAlpha),
     Colors.black.withAlpha(paintColorAlpha),
   ];
+  
+  static const int painterPathCompressionLevel = 8;
+  static const paintColorAlpha = 220;
+  static const bottomBarHeight = 50.0;
+  static const undoDisabledColor = Colors.black26;
+  static const defaultThicknessMode = 2;
+  static const bottomBarBackgroundColor = Color.fromARGB(255, 224, 224, 224);
+  static var activeEraserBackground = defaultColor.withAlpha(150);
+  static var defaultColor = Colors.amber;
 
   static const colorCircleItemSize = 50.0;
 
-  static var noteItemSeparatorColor = Colors.grey[600];
-
+  static var swipeDeleteIconSize = 40.0;
   static var swipeDeleteBackgroundColor = Colors.red.withAlpha(200);
-
   static var swipeDeleteIconColor = Colors.white;
 
-  static var swipeDeleteIconSize = 40.0;
-
-  static var noteItemVerticalPadding = 8.0;
-
   static var noteItemTitleColor = Colors.black;
-
   static var noteItemDateColor = Colors.grey[700];
-
   static var noteItemAlternativeTitleColor = Colors.grey[500];
+  static var noteItemSeparatorColor = Colors.grey[600];
 
   static String noteItemDateSameDayFormat = 'HH:mm';
-
   static String noteItemDateWithinWeekFormat = 'EEEE';
-
   static String noteItemDateMoreThanWeekFormat = 'dd/MM/yyyy';
 
 }
 
 class StringResources {
+
+  static String appTitle = 'Drawable Notepad';
+
+  // Erase whole drawing functionality
   static String eraseAlertTitle = "Are you sure?";
-
   static String eraseAlertContent = "Long pressing erase button will clear the whole drawing";
-
   static String eraseAlertYes = "Erase";
-
   static String eraseAlertNo = "Cancel";
 
+  // Note list route
   static String noteRouteToolbarTitle = "Notepad";
 
   static String addNoteFABTooltip = 'Add note';
 
-  static String appTitle = 'Drawable Notepad';
-
   static String noteItemDefaultTitle = "New note";
-
   static String noteItemHandwrittenNoteTitle = "Handwritten Note";
-
   static String noteItemDefaultAlternativeTitle = "No additional text";
-
   static String noteItemYesterdayText = "Yesterday";
-
-
 }
